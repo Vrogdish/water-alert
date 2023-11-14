11ec98f4d3396c51056c5b528292d7ddba050314
Vrogdish <vrogdish@outlook.com>
2023-11-14 13:47:02 +0100

    add station description

--- src/components/station-title/StationTitle.tsx
+++ src/components/station-title/StationTitle.tsx
@@ -2,17 +2,17 @@ import { StationType } from "@/types/StationType";
 import React from "react";
 
 interface Props {
-  station :StationType
+  station: StationType;
 }
 
 export default async function StationTitle({ station }: Props) {
-
-  
-    return (
-      <h1 className="my-20 px-6 md:text-3xl text-center">
+  return (
+    <div className="my-20">
+      <h1 className=" px-6 md:text-3xl text-center mb-6">
         Résultats d&apos;analyse de la commune : {station.nom_commune} (
         {station.code_departement})
       </h1>
-    );
-  }
-
+      <p className="text-center">{station.noms_masse_eau_edl}</p>
+    </div>
+  );
+}
